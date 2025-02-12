b6bf4d4da427c9433dd592f1e415200724ee3ea7
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2025-02-12 16:59:47 +0600

    Almost finish ERC-721

--- YT/Expert/README.md
+++ YT/Expert/README.md
@@ -1,9 +1,9 @@
 - [Learning Solidity from YT as An Expert](#learning-solidity-from-yt-as-an-expert)
-  - [ERC-721](#erc-721)
+  - [Build ERC-721 Contract](#build-erc-721-contract)
 
 # Learning Solidity from YT as An Expert
 
-## ERC-721
+## Build ERC-721 Contract
 
 - Todo list:
   - Add public mint function
@@ -11,5 +11,21 @@
   - Add supply limit
   - Add allowlist mint
   - Clean up code
-  - Add withdraw function
-- 
+  - Add withdraw functionq
+- First visit [OpenZeppelin Smart Contract Wizard](https://wizard.openzeppelin.com/)
+- It's an interactive smart contract generator based on OpenZeppelin Contracts.
+- Select `ERC721` tab
+- Enable some features like:
+  - `Mintable` - Privileged account will be able to emit new tokens
+  - `Auto Increment Ids` - New tokens will be automatically an incremental id
+  - `Burnable` - Token holders will be able to destroy their tokens
+  - `Pausable` - Allow to pause the contract/mint
+  - `Enumerable` - Allows to keep track of how many ntfs have minted so far
+  - `URI Storage` - Allows updating token URIs for individual token IDs.
+- I can see the smart contract in `Web3Builders.sol` file
+- After deployment, I can view the smart contract in the test **OpenSea** website. [see more](https://testnets.opensea.io/)
+- Need to login using **Metamask**
+- Copy the contract address from Etherscan
+- ![Etherscan For ERC721](photo/ERC721-contract-address.png)
+- Search Test OpenSea using that contract address
+- Start 46:00 minutes
---
+++ YT/Expert/Web3Builders.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.26;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Web3Builders is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
+    uint256 maxSupply = 2000;
+
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = false;
+
+    mapping(address => bool) allowList;
+
+    constructor()
+        ERC721("Web3Builders", "WE3")
+        Ownable(msg.sender)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // Add payment
+    // Add limiting of supply
+    function publicMint() public payable {
+        require(publicMintOpen, 'Public Mint Closed!');
+        require(msg.value == 0.01 ether, 'Not Enough Funds!');
+
+        internalMint();
+    }
+
+    // Add publicMint & allowListMintOpen variables
+    // Require only the allowList people to mint
+    function allowListMint() public payable {
+        require(allowListMintOpen, 'AllowList Mint Closed!');
+        require(allowList[msg.sender], 'You Are Not On The Allow List!');
+        require(msg.value == 0.001 ether, 'Not Enough Funds!');
+        
+        internalMint();
+    }
+
+    function internalMint() internal {
+        require(totalSupply() < maxSupply, 'We Are Sold Out!');
+        
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function withdraw(address _addr) external onlyOwner {
+        // Get the balance of the contract
+        uint256 balance = address(this).balance;
+        payable(_addr).transfer(balance);
+    }
+
+    // Modify the mint windows
+    function editMintWindows(
+        bool _publicMintOpen,
+        bool _allowListMintOpen
+    ) external onlyOwner {
+        publicMintOpen = _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
+    }
+
+    // Populate the 'allowList'
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i = 0; i < addresses.length; i++) {
+            allowList[addresses[i]] = true;
+        }
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
